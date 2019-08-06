1cfb37fce6a8eb0054c5eca84c5b170ccb270058
Sarath Damaraju <sarathdamaraju@hotmail.com>
2019-08-06 15:19:21 +0530

    [FIX] UI Breakage and JS exceptions in handling query params

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,14 @@ document.onreadystatechange = function () {
         let finder = new RegExp('.*url=', 'gi');
         let url = search.filter((each) => each.match(finder));
 
-        // EDGE CASE - if query params has multiple url params, consider only first
-        url = url[0].replace(finder, '')
+        if (url.length > 0) {
+          // EDGE CASE - if query params has multiple url params, consider only first
+          url = url[0].replace(finder, '')
 
-        if (url.trim().length > 0) {
-          $('.input')[0].value = url;
-          $('#submit').click();
+          if (url.trim().length > 0) {
+            $('.input')[0].value = url;
+            $('#submit').click();
+          }
         }
       }
     }, 1000);
@@ -82,8 +84,8 @@ function getData(form) {
         if (res.isShortUrl === true) {
           $('#convertedURL').text(res.url);
           $('#copy-container').show();
-        } else { 
-          $('#convertedURL').text("😑 Thats not a short URL"); 
+        } else {
+          $('#convertedURL').text("😑 Thats not a short URL");
           $('#copy-container').hide();
         }
 
